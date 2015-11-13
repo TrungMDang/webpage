075cfb8fa9daa309bf62436d275a1e6d59a3f73c
staciema <staciemashnitskaya@D-140-142-172-66.dhcp4.washington.edu>
2015-11-12 18:26:12 -0800

    stupid
    I HATE JAVA

--- Java Works/Easter Egg/src/EasterEggMain.java
+++ Java Works/Easter Egg/src/EasterEggMain.java
@@ -4,6 +4,7 @@ public class EasterEggMain {
 	public static void main(String[] args) {
 		System.out.println("Testing...");
 		System.out.println("Testing...STACIE");
+		System.out.println("Testing...STACIE_I_HATE_ECLIPSE"); 
 		System.out.println("Test for Emrullah");
 		System.out.println("Test for revert Emrullah");
 
