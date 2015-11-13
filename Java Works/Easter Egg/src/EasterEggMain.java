8c3a905bfb480c1cfabae22b1f7074131984d1d2
ecelikuw <ecelik@uw.edu>
2015-11-12 17:58:29 -0800

    emrullah

--- Java Works/Easter Egg/src/EasterEggMain.java
+++ Java Works/Easter Egg/src/EasterEggMain.java
@@ -4,6 +4,7 @@ public class EasterEggMain {
 	public static void main(String[] args) {
 		System.out.println("Testing...");
 		System.out.println("Testing...STACIE");
+		System.out.println("Test for Emrullah");
 
 	}
 
