f862c722c7ad8877d59dd01572f519b9d08a24a9
ecelikuw <ecelik@uw.edu>
2015-11-12 18:01:02 -0800

    Revert "emrullah"
    This reverts commit 8c3a905bfb480c1cfabae22b1f7074131984d1d2.

--- Java Works/Easter Egg/src/EasterEggMain.java
+++ Java Works/Easter Egg/src/EasterEggMain.java
@@ -4,7 +4,6 @@ public class EasterEggMain {
 	public static void main(String[] args) {
 		System.out.println("Testing...");
 		System.out.println("Testing...STACIE");
-		System.out.println("Test for Emrullah");
 
 	}
 
